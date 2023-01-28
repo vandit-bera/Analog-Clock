0473790383708b4f389dc7e200020b0f41471394
Vandit Bera <vanditbera@gmail.com>
2023-01-28 18:13:25 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const secHand = document.querySelector(".sec-hand");
+const minHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
+
+const setDate = () => {
+  const now = new Date();
+
+  const seconds = now.getSeconds();
+  const secondDegrees = (seconds / 60) * 360 + 90;
+  secHand.style.transform = `rotate(${secondDegrees}deg)`;
+
+  const mins = now.getMinutes();
+  const minsDegrees = (mins / 60) * 360 + (seconds / 60) * 6 + 90;
+  minHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+  const hour = now.getHours();
+  const hourDegrees = (hour / 12) * 360 + (mins / 60) * 30 + 90;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+};
+
+setInterval(setDate, 1000);
+
+setDate();
